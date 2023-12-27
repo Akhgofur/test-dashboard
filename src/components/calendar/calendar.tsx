5860601b0f60eace591acf0d715e76197e243ebf
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2023-12-27 13:55:53 +0300

    done

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -3,8 +3,11 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
 export const Calendar = () => {
+
+
+
   return (
-    <Box sx={{ p: 3, borderRadius: "12px", bgcolor: "#fff" }}>
+    <Box sx={{ p: 3, borderRadius: "12px", bgcolor: "#fff", fontSize: "16px !important" }}>
       <FullCalendar  plugins={[dayGridPlugin]} headerToolbar={{start: "title", center: "", end: "prev next"}} initialView="dayGridMonth" />-
     </Box>
   );
